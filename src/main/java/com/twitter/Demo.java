148ad289379512f56e91e9b4a4a7364ee8e4435d
Hao Luo <hluo@twitter.com>
2018-04-24 12:13:37 -0700

    Add online Analyzer

---
+++ src/main/java/com/twitter/Demo.java
@@ -0,0 +1,18 @@
+package com.twitter;
+
+import com.twitter.query.QueryAnalysis;
+import com.twitter.query.oline.OnlineQueryAnalyzer;
+
+/**
+ * Hello world!
+ */
+public class Demo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+        OnlineQueryAnalyzer analyzer = new OnlineQueryAnalyzer();
+        QueryAnalysis analysis = analyzer.analyze("select * from a");
+        analysis.print();
+    }
+}
